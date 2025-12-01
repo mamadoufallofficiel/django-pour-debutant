3b0a70a156108f7a373ca9cb10bb2070e554f317
fallisme <mamadou.fallone@gmail.com>
2025-12-01 01:45:30 +0100

    version 4 : modifier et supprimer un nouvel employé

--- employer/urls.py
+++ employer/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.liste_employers, name='liste_employers'),
     path('ajouter/', views.ajouter_employer, name='ajouter_employer'),
+    path('modifier/<int:id>/', views.modifier_employer, name='modifier_employer'),
+    path('supprimer/<int:id>/', views.supprimer_employer, name='supprimer_employer'),
 ]
--- employer/views.py
+++ employer/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Employer
 from .forms import EmployerForm
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 
 def liste_employers(request):
@@ -14,3 +14,18 @@ def ajouter_employer(request):
         form.save()
         return redirect('liste_employers')
     return render(request, 'employer/formulaire.html', {'form': form})
+
+def modifier_employer(request, id):
+    employer = get_object_or_404(Employer, id=id)
+    form = EmployerForm(request.POST or None, instance=employer)
+    if form.is_valid():
+        form.save()
+        return redirect('liste_employers')
+    return render(request, 'employer/formulaire.html', {'form': form})
+
+def supprimer_employer(request, id):
+    employer = get_object_or_404(Employer, id=id)
+    if request.method == 'POST':
+        employer.delete()
+        return redirect('liste_employers')
+    return render(request, 'employer/confirmer_suppression.html', {'employer': employer})
